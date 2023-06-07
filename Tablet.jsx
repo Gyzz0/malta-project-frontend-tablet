97b98ab55e1c388cf67323018a234a28a979f0d5
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-07 16:18:53 +0200

    Prima schermata

---
+++ Tablet.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function Tablet() {
+  return (
+    <>
+    <Container fluid>
+      <h1>Tablet</h1>
+    </Container>
+    </>
+  );
+}
