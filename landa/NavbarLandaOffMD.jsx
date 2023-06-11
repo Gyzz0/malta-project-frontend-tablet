29a6b4cee94ce402e558895d2cca7dc318e14ade
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-11 15:04:09 +0200

    Creata NavbarHomeOffMD

---
+++ landa/NavbarLandaOffMD.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { Col } from "react-bootstrap";
+import GenericModal from "../../../assets/components/GenericModal";
+
+export default function NavbarLandaOffMD(props) {
+  const [modalIndex, setModalIndex]=useState(null);
+
+  const sezioni = [
+    "Guida",
+    "Manuale",
+    "Logo",
+    "Regolamento",
+    "Presenti"
+  ];
+
+  const componenti = [
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]
+
+  const defaultClass = "text-center h5 d-inline-block py-2 rounded";
+
+  return (
+    <>
+      {sezioni.map((item, index) => {
+        return item != "Logo" ? (
+          <Col
+            key={index}
+            md={{ span: 2 }}
+            xl={{ span: 2 }}
+            className={defaultClass+" clickable hover-bg-borgogna"}
+            onClick={() => setModalIndex(index)}
+          >
+            {item}
+          </Col>
+        ) : (
+          <Col 
+            key={index}
+            md={2} 
+            xl={4} 
+            className={defaultClass}
+          >
+            <img width={350} height={50} />
+          </Col>
+        );
+      })}
+
+      {
+        modalIndex!=null //GUIDA
+          ?<GenericModal handleClose={()=>setModalIndex(null)} titolo={sezioni[modalIndex]} componente={componenti[modalIndex]}/>
+          :null
+      }
+    </>
+  );
+}
